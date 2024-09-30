ffead11cf72cb4432bb9a2db3b9275ad8d3cd198
nomanbrohi <nomanbrohi9@gmail.com>
2024-09-30 16:48:43 +0500

    Adjustment for responsive design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,8 +58,8 @@ export default function CountdownTimer() {
 
   return (
     <div className="flex h-screen justify-center items-center bg-gradient-to-r from-blue-500 to-purple-600">
-      <div className="w-1/3 h-2/4 flex flex-col justify-center items-center bg-white rounded-3xl shadow-lg p-8">
-        <h1 className="text-2xl font-bold text-gray-700 mb-4">Countdown Timer</h1>
+      <div className="w-full sm:w-3/4 md:w-1/2 lg:w-1/3 h-auto flex flex-col justify-center items-center bg-white rounded-3xl shadow-lg p-8">
+        <h1 className="text-2xl sm:text-2xl md:text-3xl  font-bold text-gray-700 mb-4">Countdown Timer</h1>
         
         {/* Input for timer */}
         <Input
@@ -76,7 +76,7 @@ export default function CountdownTimer() {
         </div>
 
         {/* Control buttons */}
-        <div className="flex space-x-4">
+        <div className="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-4">
           <Button
             onClick={handleStart}
             className="px-6 py-2 bg-slate-600 text-white rounded-full hover:bg-yellow-600 transition"
